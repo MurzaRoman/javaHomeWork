979c7d09d93be5fe32c674b8f45715adbdc77451
Roman Murza <47818076+MurzaRoman@users.noreply.github.com>
2019-04-21 23:46:40 +0300

    Delete Main.java

--- HOMEWORK/Main.java
+++
@@ -1,8 +0,0 @@
-package HomeWork6;
-
-public class Main {
-    public static void main(String[] args) {
-        ControllerBooks controllerBooks = new ControllerBooks();
-        controllerBooks.perforance();
-    }
-}
